88f2f326e8122792714d9590a8fafc352926b358
lain <sandrokalett@gmail.com>
2021-12-10 17:48:26 +0100

    added animations

---
+++ Resources/Asset/animation/animation.py
@@ -0,0 +1,13 @@
+import pygame
+
+startup_animation = [pygame.image.load("Resources/Asset/animation/startup/p0.png"),
+                     pygame.image.load("Resources/Asset/animation/startup/p1.png"),
+                     pygame.image.load("Resources/Asset/animation/startup/p2.png"),
+                     pygame.image.load("Resources/Asset/animation/startup/p3.png"),
+                     pygame.image.load("Resources/Asset/animation/startup/p4.png"),
+                     pygame.image.load("Resources/Asset/animation/startup/p5.png"),
+                     pygame.image.load("Resources/Asset/animation/startup/p6.png")]
+
+options_animation = [pygame.image.load("Resources/Asset/animation/options_menu/a1.png"),
+                     pygame.image.load("Resources/Asset/animation/options_menu/a2.png"),
+                     pygame.image.load("Resources/Asset/animation/options_menu/a3.png")]
