5b8686f3c384c54f2b9646d20578571c74a4fe52
lain <sandrokalett@gmail.com>
2021-12-13 23:06:00 +0100

    added asset_loader.py

---
+++ Resources/Asset/asset_loader.py
@@ -0,0 +1,4 @@
+import pygame
+main_menu_bg = pygame.image.load("Resources/Asset/background/main_menu_bg.png")
+main_options_bg = pygame.image.load("Resources/Asset/background/main_options_bg.png")
+main_stats_bg = pygame.image.load("Resources/Asset/background/main_stats_bg.png")
