00c9de8483edd5177f5de7dfd1aa945d70a2c68c
lain <sandrokalett@gmail.com>
2021-12-16 18:08:29 +0100

    add comments

--- Resources/Asset/asset_loader.py
+++ Resources/Asset/asset_loader.py
@@ -1,4 +1,5 @@
 import pygame
+# Load all images into variables
 main_menu_bg = pygame.image.load("Resources/Asset/background/main_menu_bg.png")
 main_options_bg = pygame.image.load("Resources/Asset/background/main_options_bg.png")
 main_stats_bg = pygame.image.load("Resources/Asset/background/main_stats_bg.png")
